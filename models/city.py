020a5188870bb527f82f7515ac8427924b6c0e3e
ShadrachKiplangat <55084539+sherdooh@users.noreply.github.com>
2023-06-21 13:46:57 +0300

    Update unit tests city.py

--- models/city.py
+++ models/city.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
-"""City class"""
+""" City Module for HBNB project """
+from os import getenv
+import sqlalchemy
+import models
+from models.base_model import Base
 from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
-    """class City inherits from BaseModel"""
-
-    state_id = ""
-
-    name = ""
+class City(BaseModel, Base):
+    """ The city class, contains state ID and name """
+    if models.storage_t == "db":
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        name = Column(String(128), nullable=False)
+        __tablename__ = "cities"
+        places = relationship("Place", backref="cities")
+    else:
+        state_id = ""
+        name = ""
 
     def __init__(self, *args, **kwargs):
-
-        """class constructor"""
+        """initializes city"""
         super().__init__(*args, **kwargs)
